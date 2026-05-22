af2f7f0765a9ba6661066169d7ed41c8374c981a
Fares Emad <167219814+faresdphish@users.noreply.github.com>
2026-05-22 18:57:10 +0300

    refactor: consolidate rate throttles into a base class

--- auth_system/throttles.py
+++ auth_system/throttles.py
@@ -3,49 +3,30 @@ from rest_framework.throttling import AnonRateThrottle
 from .conf import LOGIN_THROTTLE_RATES
 
 
-class LoginRateThrottle(AnonRateThrottle):
+class _BaseCustomThrottle(AnonRateThrottle):
+    def get_rate(self):
+        return None
+
+    def allow_request(self, request, view):
+        rate = LOGIN_THROTTLE_RATES.get(self.scope)
+        if rate is None:
+            return True
+        self.rate = rate
+        self.num_requests, self.duration = self.parse_rate(self.rate)
+        return super().allow_request(request, view)
+
+
+class LoginRateThrottle(_BaseCustomThrottle):
     scope = "login"
 
-    def allow_request(self, request, view):
-        rate = LOGIN_THROTTLE_RATES.get(self.scope)
-        if rate is None:
-            return True
-        self.rate = rate
-        self.num_requests, self.duration = self.parse_rate(self.rate)
-        return super().allow_request(request, view)
 
-
-class SignupRateThrottle(AnonRateThrottle):
+class SignupRateThrottle(_BaseCustomThrottle):
     scope = "signup"
 
-    def allow_request(self, request, view):
-        rate = LOGIN_THROTTLE_RATES.get(self.scope)
-        if rate is None:
-            return True
-        self.rate = rate
-        self.num_requests, self.duration = self.parse_rate(self.rate)
-        return super().allow_request(request, view)
 
-
-class PasswordResetRateThrottle(AnonRateThrottle):
+class PasswordResetRateThrottle(_BaseCustomThrottle):
     scope = "password_reset"
 
-    def allow_request(self, request, view):
-        rate = LOGIN_THROTTLE_RATES.get(self.scope)
-        if rate is None:
-            return True
-        self.rate = rate
-        self.num_requests, self.duration = self.parse_rate(self.rate)
-        return super().allow_request(request, view)
 
-
-class Verify2FARateThrottle(AnonRateThrottle):
+class Verify2FARateThrottle(_BaseCustomThrottle):
     scope = "verify_2fa"
-
-    def allow_request(self, request, view):
-        rate = LOGIN_THROTTLE_RATES.get(self.scope)
-        if rate is None:
-            return True
-        self.rate = rate
-        self.num_requests, self.duration = self.parse_rate(self.rate)
-        return super().allow_request(request, view)
